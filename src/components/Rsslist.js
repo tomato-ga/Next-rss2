f6d251a1bd8ed5cd75c8ebb5a865f974d9291aaf
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-07-17 12:25:37 +0900

    詳細ページ表示

--- src/components/Rsslist.js
+++ src/components/Rsslist.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import React from 'react';
 import Link from 'next/link';
+import shuffle from '@/lib/shuffle';
 
 const RSSList = () => {
   const [rssItems, setRSSItems] = useState([]);
@@ -8,18 +9,24 @@ const RSSList = () => {
   useEffect(() => {
     const fetchRSSItems = async () => {
       try {
-        const responseSites = await fetch('http://localhost:7002/sites');
+        const responseSites = await fetch('http://192.168.0.25:7002/rss');
         const sites = await responseSites.json();
 
         const promises = sites.map(async (site) => {
-          const response = await fetch(`http://localhost:7002/sites/${site.id}/rss/latest`);
+          const response = await fetch(`http://192.168.0.25:7002/sites/${site.id}/rss/latest`);
           const data = await response.json();
 
-          return { name: site.name, items: data.slice(0, 5), created_at: site.created_at };
+          return data.map(item => ({ ...item, siteName: site.name, siteCreatedAt: site.created_at }));
         });
 
-        const combinedRSSItems = await Promise.all(promises);
-        setRSSItems(combinedRSSItems);
+        let combinedRSSItems = await Promise.all(promises);
+        combinedRSSItems = combinedRSSItems.flat();
+
+        // シャッフル実行
+        const shuffledItems = shuffle(combinedRSSItems);
+        console.log(shuffledItems);
+
+        setRSSItems(shuffledItems);
       } catch (error) {
         console.error(error);
       }
@@ -35,39 +42,38 @@ const RSSList = () => {
 
   return (
     <div className='container mx-auto flex p-5'>
-    <div className='border border-gray-300 rounded-lg shadow-lg p-4 flex-grow-0 w-1/3'>
-      <h2 className='text-2xl mb-4'>Sidebar</h2>
+      <div className='border border-gray-300 rounded-lg shadow-lg p-4 flex-grow-0 w-1/3'>
+        <h2 className='text-2xl mb-4'>Sidebar</h2>
         <p>test</p>
       </div>
 
       <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-5 p-5">
-        {rssItems.map((site, index) => (
-          <div key={index} className='my-1 px-4 border-gray-300 rounded-lg shadow-lg'>
-            <h1 className='text-2xl py-6 text-center bg-indigo-800 text-white'>{site.name}</h1>
-            {site.items.map((item) => {
-              const date = new Date(item.published_at);
-              const formattedDate = new Intl.DateTimeFormat('ja-JP', {
-                year: 'numeric', month: '2-digit', day: '2-digit',
-                hour: '2-digit', minute: '2-digit', second: '2-digit'
-              }).format(date);
-              return (
-                <div key={item.id} className=' border-b border-gray-200'>
-                  <a href={item.link} target='_blank' rel='noopener noreferrer' className='text-blue-500 hover:text-blue-700'>
-                    <h2 className='m-3'>
-                      {item.title}
-                    </h2>
-                  </a>
-                  <Link href="/posts/[siteIndex]/[itemId]" as={`/posts/${index}/${item.id}`}>
-                    <div className="text-right"><button>詳細</button></div>
-                  </Link>
-                  <div className='date px-2 align-sub text-gray-500'>
-                    <p>{formattedDate}</p>
-                  </div>
+        {rssItems.map((item, index) => {
+          const date = new Date(item.published_at);
+          const formattedDate = new Intl.DateTimeFormat('ja-JP', {
+            year: 'numeric', month: '2-digit', day: '2-digit',
+            hour: '2-digit', minute: '2-digit', second: '2-digit'
+          }).format(date);
+          return (
+            <div key={index} className='my-1 px-4 border-gray-300 rounded-lg shadow-lg'>
+              {/* <h1 className='text-2xl py-6 text-center bg-indigo-800 text-white'>{item.siteName}</h1> */}
+              <div className='border-b border-gray-200'>
+              <Link href="/[siteId]/[itemId]" as={`/${item.site_id}/${item.id}`}>
+                <img src={item.imgurl} className='max-h-[250px]'></img>
+                {/* <a href={item.link} target='_blank' rel='noopener noreferrer' className='text-blue-500 hover:text-blue-700'> */}
+                  <h2 className='m-3 text-xl'>
+                    {item.title}
+                  </h2>
+                {/* </a> */}
+                  {/* <div className="text-right"><button>詳細</button></div> */}
+                </Link>
+                <div className='date px-2 align-sub text-gray-500'>
+                  <p>{formattedDate}</p>
                 </div>
-              );
-            })}
-          </div>
-        ))}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/pages/[siteId]/[itemId].js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router';
+import Header from '@/components/Header';
+
+const Posts = () => {
+    const [data, setData] = useState(null);
+    const [rssFeeds, setRssFeeds] = useState([]);
+    const router = useRouter();
+    const { siteId, itemId } = router.query;
+
+    useEffect(() => {
+        if (router.isReady) {
+            const fetchData = async () => {
+                try {
+                    const ENDP = `http://192.168.0.25:7002/sites/rss/${itemId}`;
+                    const res = await fetch(ENDP);
+                    const data = await res.json();
+                    setData(data);
+                    
+
+                    const feedsResponse = await fetch(`http://192.168.0.25:7002/sites/${siteId}/rss/latest`);
+                    const feedsData = await feedsResponse.json();
+                    setRssFeeds(feedsData);
+                } catch (error) {
+                    console.error("Error fetching data: ", error);
+                }
+            }
+            if (itemId) {
+                fetchData();
+            }
+        }
+    }, [router.isReady, siteId, itemId]);  // Note the addition of router.isReady
+
+    if (!data) {
+        return <p>Loading...</p>
+    }
+    console.log(data);
+
+    return (
+        <div>
+            <Header />
+            <div className='container mx-auto px-4 py-6'>
+                <h2 className="text-3xl bg-indigo-400 text-white">{data[0].title}</h2>
+                <div className='desc p-5'>
+                    {<img src={data[0].imgurl}></img>}
+                </div>
+                <div className=''>
+                    <h3><a href={data[0].link} target='_blank'>{data[0].title}</a></h3>
+                </div>
+                {/* <div className='rss-feeds'>
+                    <h3 className='text-3xl'>このサイトのRSS一覧</h3>
+                    {rssFeeds.slice(0,6).map((feed, index) => (
+                        <div key={index}>
+                            <a href={feed.link} target="_blank" rel="noopener noreferrer">
+                                <h3 className="text-2xl">{feed.title}</h3>
+                            </a>
+                        </div>
+                    ))}
+                </div> */}
+            </div>
+        </div>
+    )
+}
+
+export default Posts
