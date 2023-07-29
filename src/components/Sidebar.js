204fbb0a90ae7c3ef0c1d4a34b52e435b76a8708
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-07-29 13:20:17 +0900

    エンドポイントがおかしかったので修正 / Sidebarのルーティングがおかしかったのを修正

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,10 +6,7 @@ const Sidebar = () => {
         <div className='border border-gray-300 rounded-lg shadow-lg p-4 flex-grow-0 w-72'>
             <h2 className='text-2xl mb-4'>サイドバー</h2>
             <p>
-                <Link href='/ssr'>
-                    SSR
-                </Link>
-                <Link href='/tag/爆乳'>
+                <Link href='/tag/爆乳/page/1'>
                     爆乳
                 </Link>
             </p>
