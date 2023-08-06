a0ebe18359b3fac911e3c7073e07011125fe52ef
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-08-06 12:06:30 +0900

    CSS調整中

--- src/pages/topSSR.js
+++ src/pages/topSSR.js
@@ -51,11 +51,10 @@ export default function Ssr({ data, totalCount, page, limit }) {
       <div className='container mx-auto flex flex-col-reverse md:flex-row p-5 justify-between md:justify-start'>
           <Sidebar />
           <div className="md:w-3/4 md:ml-4 grid sm:grid-cols-1 md:grid-cols-2 gap-5 p-5 order-2 md:order-2">
-  
               {data.map((item, index) => {
                   let date = item.published_at ? new Date(item.published_at) : null;
                   let formattedDate = "";
-      
+  
                   if (date && !isNaN(date.getTime())) {
                       formattedDate = new Intl.DateTimeFormat('ja-JP', {
                           year: 'numeric', month: '2-digit', day: '2-digit',
@@ -64,23 +63,35 @@ export default function Ssr({ data, totalCount, page, limit }) {
                   } else {
                       console.error("Invalid date: ", item.published_at);
                   }
-      
+  
                   let tagsArray = [];
                   if (item.tag) {
                       tagsArray = item.tag.split(',').map(tag => tag.trim());
                   }
-      
+  
                   return (
-                      <div key={index} className='Ui w-96 h-96 relative'>
-                          <div onClick={() => {handleClickCount(item.id)}}>
+                      <div key={index} className='Ui w-96 h-auto p-4 bg-white shadow-md rounded-md'>
+                          <div onClick={() => { handleClickCount(item.id) }}>
                               <Link href="/[siteId]/[itemId]" as={`/${item.site_id}/${item.id}`}>
-                                  <div className="Topimages w-96 h-56 left-0 top-0 absolute bg-pink-100 rounded-sm">
-                                      <Image fill src={item.imgurl} className={styles.image} alt={item.title} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
-                                  </div>
-                                  <div className="Titles w-96 h-36 left-0 top-[225px] absolute">
-                                      <div className="Rectangle5 w-96 h-28 left-0 top-0 absolute bg-white" />
-                                      <div className=" w-96 h-20 left-[28px] top-[34px] absolute text-black text-2xl font-normal">
+                                  <div>
+                                      <div className="Topimages w-full h-56 bg-white-100 rounded-sm mb-4">
+                                        <Image fill src={item.imgurl} className={styles.image} alt={item.title} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
+                                      </div>
+                                      <div className="Titles text-black text-2xl font-normal mb-4">
                                           {item.title}
+                                      </div>
+  
+                                      <div className="Tags text-black text-2xl font-normal mb-4">
+                                          <Tags tagsArray={tagsArray} />
+                                      </div>
+  
+                                      <div className="Clicks text-center mb-4">
+                                          <span style={{ color: 'black', fontSize: '24px', fontWeight: 'normal', letterSpacing: '0.1em' }}>
+                                              {clickCounts[item.id] || 0}
+                                          </span>
+                                          <span style={{ color: 'black', fontSize: '16px', fontWeight: 'normal', letterSpacing: '0.1em' }}>
+                                              Click
+                                          </span>
                                       </div>
                                   </div>
                               </Link>
@@ -92,4 +103,8 @@ export default function Ssr({ data, totalCount, page, limit }) {
       </div>
   );
   
+  
+  
 }
+
+// TODO タグとクリックCSSいれる
