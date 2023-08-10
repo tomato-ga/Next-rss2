076e58a7bcb160baf1f88e034c1b32684c118ce7
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-08-10 23:14:32 +0900

    タグのスペース削除

--- src/components/Localrireki.js
+++ src/components/Localrireki.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+import { handleClickCount } from '@/lib/clickCountDB';
+
 
 const Localrireki = ({ localData }) => {
     return (
@@ -7,14 +9,18 @@ const Localrireki = ({ localData }) => {
             {localData ? localData.slice(0, 5).map((articleGroup, groupIndex) => (
             <div key={groupIndex} className="w-full md:w-1/2 lg:w-auto mb-3">
                 {articleGroup.map((article, articleIndex) => (
-                <Link key={articleIndex} href="/[siteId]/[itemId]" as={`/${article.site_id}/${article.id}`}>
-                    <div className="mr-3 flex flex-col lg:flex-inline">
-                    <div className="relative flex justify-center items-center h-[200px] md:h-[170px] w-[250px] md:w-[170px] mx-auto">
-                        <Image className="" fill src={article.imgurl} alt={article.title} sizes="(max-width: 600px) 50vw, (max-width: 768px) 50vw, (max-width: 1200px) 50vw" />
-                    </div>
-                    <h3 className='p-2 text-center text-base'>{article.title}</h3>
-                    </div>
-                </Link>
+                <div onClick={() => {handleClickCount(article.id)}}>
+
+                    <Link key={articleIndex} href="/[siteId]/[itemId]" as={`/${article.site_id}/${article.id}`}>
+                        <div className="mr-3 flex flex-col lg:flex-inline">
+                        <div className="relative flex justify-center items-center h-[200px] md:h-[170px] w-[250px] md:w-[170px] mx-auto">
+                            <Image className="" fill src={article.imgurl} alt={article.title} sizes="(max-width: 600px) 50vw, (max-width: 768px) 50vw, (max-width: 1200px) 50vw" />
+                        </div>
+                        <h3 className='p-2 text-center text-base'>{article.title}</h3>
+                        </div>
+                    </Link>
+
+                </div>
                 ))}
             </div>
             )) : null}
--- src/components/Tags.js
+++ src/components/Tags.js
@@ -3,17 +3,20 @@ const { default: Link } = require("next/link");
 
 const Tags = ({ tagsArray, numberTags }) => {
 
-    return (
-        <div className='tags flex sm:m-auto flex-wrap p-2'>
-            {tagsArray.slice(0,numberTags).map((tag, tagIndex) => (
-                <Link key={tagIndex} href="/tag/[tagpage]/page/[pageNumber]" as={`/tag/${tag}/page/1`}>
-                    <span className="inline-block bg-pink-50 rounded-full px-3 py-1 text-xs md:text-sm font-semibold text-pink-600 mr-2 mb-2">
-                        #{tag}
-                    </span>
-                </Link>
-            ))}
-        </div>
-    );
+const cleanedTags = tagsArray.map(tag => tag.trim());
+
+return (
+    <div className='tags flex sm:m-auto flex-wrap p-2'>
+        {cleanedTags.slice(0, numberTags).map((tag, tagIndex) => (
+            <Link key={tagIndex} href="/tag/[tagpage]/page/[pageNumber]" as={`/tag/${tag}/page/1`}>
+                <span className="inline-block bg-pink-50 rounded-full px-3 py-1 text-xs md:text-sm font-semibold text-pink-600 mr-2 mb-2">
+                    #{tag}
+                </span>
+            </Link>
+        ))}
+    </div>
+);
+
 
 };
 
