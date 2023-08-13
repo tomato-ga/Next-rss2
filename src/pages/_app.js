4a10d53786ea82411921135730588378a39666bd
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-08-13 14:00:19 +0900

    GA設定

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,48 +1,32 @@
-import { useRouter } from 'next/router';
 import '@/styles/globals.css';
 import { DefaultSeo } from 'next-seo';
 import SEO from 'next-seo.config.js';
-import Script from 'next/script';
-import * as gtag from "@/lib/gtag";
-
+import Head from 'next/head';
 
 
 export default function App({ Component, pageProps }) {
 
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouterChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouterChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouterChange);
-    };
-  }, [router.events]);
 
   return (
     <>
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-0PFN7Q7ZBX`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0PFN7Q7ZBX');
+            `,
+          }}
+        />
+      </Head>
+
       <DefaultSeo {...SEO} />
-      
-      <Script 
-      strategy="afterInteractive" 
-      src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
-      />
-
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${gtag.GA_MEASUREMENT_ID}');
-          `,
-        }}
-      />
-
       <Component {...pageProps} />
     </>
   )
