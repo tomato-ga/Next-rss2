5c1ab7cf41aab497cb81b7877ecad6e7a5af7cc3
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-08-13 14:34:05 +0900

    ga設定中

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,25 @@ import '@/styles/globals.css';
 import { DefaultSeo } from 'next-seo';
 import SEO from 'next-seo.config.js';
 import GoogleTagManager from '@/lib/gtag';
-
+import { pageview } from '@/lib/gaPv';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
 
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
