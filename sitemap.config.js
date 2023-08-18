00fcd0f870b38cf5edd7bafd7e4ad1c90c641c33
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-08-19 08:06:03 +0900

    ssr sitemap

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,8 +1,14 @@
-// sitemap.config.js
-
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://erorice.com',
-  generateRobotsTxt: false,
+  generateRobotsTxt: true, // これをtrueに設定します
+  exclude: ['/server-sitemap.xml'], // これを追加します
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://erorice.com/server-sitemap.xml', // ここを追加します
+    ],
+  },
+  // 以下の設定はそのまま保持します
   sitemapSize: 7000,
   outDir: './out',
 };
---
+++ src/pages/server-sitemap.xml/index.js
@@ -0,0 +1,37 @@
+import { getServerSideSitemapLegacy } from 'next-sitemap';
+import axios from 'axios';
+
+export const getServerSideProps = async (ctx) => {
+  let urls = [];
+
+  // 1から1000までのページを取得する例
+  for (let number = 1; number <= 2800; number++) {
+    try {
+      const response = await axios.get(`https://api.erorice.com/rss/all/latest?page=${number}`);
+
+      // ステータスコードが200以外の場合、ループを中断
+      if (response.status !== 200) {
+        console.error(`Error: API responded with status code ${response.status}`);
+        break;
+      }
+
+      const data = response.data;
+
+      // APIレスポンスのデータ構造に合わせて、動的なパスを抽出します。
+      const newUrls = data.items.map(item => ({
+        loc: `https://erorice.com/${item.site_id}/${item.id}`, // Absolute url
+        lastmod: new Date().toISOString(),
+      }));
+
+      urls = urls.concat(newUrls);
+    } catch (error) {
+      console.error('Failed to get dynamic routes:', error);
+      break;
+    }
+  }
+
+  return getServerSideSitemapLegacy(ctx, urls);
+};
+
+// Default export to prevent next.js errors
+export default function Sitemap() {}
