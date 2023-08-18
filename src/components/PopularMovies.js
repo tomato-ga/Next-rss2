ca154ce434c1cf7408d1ab1e5501e668f2c23fc7
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-08-18 22:24:45 +0900

    トップ人気動画をImageコンポーネントからimgタグへ変更

--- src/components/PopularMovies.js
+++ src/components/PopularMovies.js
@@ -24,31 +24,61 @@ const PopularMovies = ({ popumoviesRes }) => {
     // }, [])
 
 
+
     return (
-<div className="justify-center text-center font-bold">
-    <h2 className="mb-4">
-        <span className="border-b-2 pb-1 border-blue-300 text-2xl">
-            <Link href="/Mostpopular">
-            人気動画
-            </Link>
-        </span>
-    </h2>
-    
-    <div className="flex flex-wrap justify-center sm:grid-cols-2 lg:flex gap-3">
-        {popumoviesRes.map((movie, movieIndex) => (
-            <div key={movieIndex} className="mr-3 mb-3 flex flex-col">
-                <div className="relative flex justify-center items-center h-[150px] w-[160px] md:h-[170px] md:w-[180px] mx-auto">
-                    <Link href="/[siteId]/[itemId]" as={`/${movie.rss.site_id}/${movie.rss.id}`}>
-                        <Image fill src={movie.rss.imgurl} alt={movie.rss.title}  />
+        <div className="justify-center text-center font-bold">
+            <h2 className="mb-4">
+                <span className="border-b-2 pb-1 border-blue-300 text-2xl">
+                    <Link href="/Mostpopular">
+                    人気動画
                     </Link>
-                </div>
+                </span>
+            </h2>
+            
+            <div className="flex flex-wrap justify-center sm:grid-cols-2 lg:flex gap-3">
+                {popumoviesRes.map((movie, movieIndex) => (
+                    <div key={movieIndex} className="mr-3 mb-3 flex flex-col">
+                        <div className="relative w-[160px] h-[150px] md:w-[180px] md:h-[170px] mx-auto">
+                            <Link href="/[siteId]/[itemId]" as={`/${movie.rss.site_id}/${movie.rss.id}`}>
+                                <div className="absolute inset-0">
+                                    <img src={movie.rss.imgurl} className="w-full h-full object-cover" alt={movie.rss.title} />
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                ))}
             </div>
 
-        ))}
-    </div>
-</div>
+        </div>
+    );
+    
+// 2023/08/18 Imageコンポーネントを削除してimgタグへ変更した
 
-);
+// ImageコンポーネントのときのJSX
+//     return (
+//     <div className="justify-center text-center font-bold">
+//         <h2 className="mb-4">
+//             <span className="border-b-2 pb-1 border-blue-300 text-2xl">
+//                 <Link href="/Mostpopular">
+//                 人気動画
+//                 </Link>
+//             </span>
+//         </h2>
+        
+//         <div className="flex flex-wrap justify-center sm:grid-cols-2 lg:flex gap-3">
+//             {popumoviesRes.map((movie, movieIndex) => (
+//                 <div key={movieIndex} className="mr-3 mb-3 flex flex-col">
+//                     <div className="relative flex justify-center items-center h-[150px] w-[160px] md:h-[170px] md:w-[180px] mx-auto">
+//                         <Link href="/[siteId]/[itemId]" as={`/${movie.rss.site_id}/${movie.rss.id}`}>
+//                             <Image fill src={movie.rss.imgurl} alt={movie.rss.title}  />
+//                         </Link>
+//                     </div>
+//                 </div>
+
+//             ))}
+//         </div>
+//     </div>
+// );
 }
 
 
