2a7b4e6d0f951e94f800d07982f886c553a5bd89
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-08-19 01:59:48 +0900

    ひとまず

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['dr3jjw5otuz25.cloudfront.net'],
-  }
+    unoptimized: true
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
