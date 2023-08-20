cd4b92e908863452fdf20c11963fcc349386e200
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-08-20 13:52:39 +0900

    npm uninstall @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -8,8 +8,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig;
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-module.exports = withBundleAnalyzer({})
