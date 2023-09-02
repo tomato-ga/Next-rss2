74aed5969449988bddd72883b7d55bcb117c1daf
tomato <52771764+tomato-ga@users.noreply.github.com>
2023-09-02 16:39:21 +0900

    Goodボタンを個別ページの関連記事(RelatedTagposts)に追加

--- src/components/RelatedTagPosts.js
+++ src/components/RelatedTagPosts.js
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import styles from './iiim.module.css'
 import FetchClickCounts from './Clickcount';
 import { handleClickCount } from '@/lib/clickCountDB';
+import Favthumbnail from './Favthumbnail';
 
 
 const RelatedTagPosts = ({ tag }) => {
@@ -40,17 +41,34 @@ const RelatedTagPosts = ({ tag }) => {
             return (
                 <div key={post.id} className="border-gray-300 rounded-lg shadow-lg">
                   <div onClick={() => { handleClickCount(post.id) }}>
-                    <Link href="/item/[items]" as={`/item/${post.id}`}>
-                    <div className="relative h-[270px] w-full md:h-[320px] md:w-full">
-                                      <img src={post.imgurl} className="w-full h-full object-cover" alt={post.title} />
-              
-                                      <span className="absolute rounded-md top-2 left-2 bg-white bg-opacity-90 text-red-600 text-xl font-bold tracking-widest text-center p-1">
-                                          <FetchClickCounts itemId={post.id} />
-                                      </span>
-                    </div>
-                    <p className="m-2 text-sm md:text-xl font-bold text-blue-600">{post.title}</p>
-                    </Link>
-                    <Tags tagsArray={tagsArray} numberTags={5} />
+                  <div className="h-[270px] w-full md:h-[320px] md:w-full">
+                                    <Link href="/item/[items]" as={`/item/${post.id}`} className=''>
+                                        <img src={post.imgurl} className="w-full h-full object-cover" alt={post.title} />
+                                    </Link>
+                                </div>
+
+                                <div className='relative flex items-center mt-2'>
+                                    <div className='flex flex-col items-center'>
+                                        <span className="text-red-600 text-xl font-bold tracking-widest text-center p-2">
+                                            <FetchClickCounts itemId={post.id} />
+                                        </span>
+
+                                        <span className="text-red-600 text-xl font-bold tracking-widest text-center p-2">
+                                            <Favthumbnail postId={post.id} articleData={post}/>
+                                        </span>
+                                    </div>
+
+                                    <div className='ml-4'>
+                                        <Link href="/item/[items]" as={`/item/${post.id}`} className=''>
+                                            <h2 className='text-sm md:text-xl font-bold text-blue-600 z-20'>
+                                                {post.title}
+                                            </h2>
+                                        </Link>
+                                        <div className='tags'>
+                                            <Tags tagsArray={tagsArray} numberTags={5} />
+                                        </div>
+                                    </div>
+                                </div>
                 </div>
                 </div>
             )
